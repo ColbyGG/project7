6cefa3d9d9e136232109b60fd59c392a0503e766
Colby Green <cjdude22@gmail.com>
2016-12-12 10:28:21 -0800

    Parts 1+2

--- project7.js
+++ project7.js
@@ -12,9 +12,20 @@ function transformPoint(event) {
 
 // Step 2: drawSquare and drawCircle functions
 function drawSquare(x, y, size, color) {
-  // square drawing code here
+  var square = document.createElementNS(namespace,"rect");
+  square.setAttribute("x",x);
+  square.setAttribute("y",y);
+  square.setAttribute("width",size);
+  square.setAttribute("height",size);
+  square.setAttribute("fill", color);
 }
-
+function drawCircle(x,y,size,color){
+  var circle = document.createElementNS(namspace,"circle");
+  circle.setAttribute("cx",x);
+  circle.setAttribute("cy",y);
+  circle.setAttribute("r", size);
+  circle.setAttribute("fill",color);
+}
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
   // what do you want to do when the user presses down
